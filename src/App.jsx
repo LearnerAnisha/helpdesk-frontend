8bdf03071d179f2172d9a2bfb36588c11a1d6c37
anisha <anishagautam012@gmail.com>
2025-03-14 17:59:41 +0530

    Initialized project with Vite and configured Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-dvh flex items-center justify-center font-bold font-sans text-3xl'>
+      Welcome to Helpdesk.
+    </div>
+  )
+}
+
+export default App
