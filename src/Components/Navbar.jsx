2ab36a0dea39dfb04de12762f731576b125da514
anisha <anishagautam012@gmail.com>
2025-03-16 12:46:24 +0530

    made navbar more customizeable

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,31 @@
-import { Outlet, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import clsx from "clsx"; // Import clsx for class merging
 
-const Navbar = () => {
+const Navbar = ({
+    logo = "Help Desk!",
+    links = [{ name: "Home", path: "/" }, { name: "Authorize", path: "/auth" }],
+    classes = ""
+}) => {
     return (
-        <div className="fixed top-5 w-full right-5">
-            <nav>
-                <Link to="/">Home</Link> | <Link to="/auth">Login/Register</Link> | <Link to="/dashboard">Dashboard</Link>
-                | <Link to="/notfound">Not Found</Link>
-            </nav>
-        </div>
+
+        <nav className={clsx("fixed top-3 left-[50%] translate-x-[-50%] w-full sm:w-[60vw] h-[50px] sm:h-[70px] flex items-center justify-between !px-2 !sm:px-10  shadow-lg backdrop-blur-[5px] bg-[rgba(255,255,255,0.2)] rounded-md", classes)}>
+            {logo && <Link to="/" className="text-xl font-semibold sm:w-[30%]">{logo}</Link>}
+            <div className="grow flex justify-end gap-9">
+                {links
+                    .filter(link => link.name && link.path) // Ensure valid links
+                    .map((link, index) => (
+                        <Link
+                            key={index}
+                            to={link.path}
+                            className="hover:text-blue-300 transition-colors"
+                        >
+                            {link.icon} {link.name}
+                        </Link>
+                    ))
+                }
+            </div>
+        </nav>
+
     );
 };
 
